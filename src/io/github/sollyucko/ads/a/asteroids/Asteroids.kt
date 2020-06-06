66a9123cc6cd2ee77d1c7273c8bc07fe086f494c
Solomon Ucko <solly.ucko@gmail.com>
2020-06-06 11:51:30 -0400

    Increase chance of spawning a new asteroid

--- src/io/github/sollyucko/ads/a/asteroids/Asteroids.kt
+++ src/io/github/sollyucko/ads/a/asteroids/Asteroids.kt
@@ -55,7 +55,7 @@ class Asteroids : GameCanvas("Asteroids!", 800, 600) {
                         asteroids.add(Asteroid.createRandomNear(asteroid.anchor, this, asteroid.size / 2.0))
                         asteroids.add(Asteroid.createRandomNear(asteroid.anchor, this, asteroid.size / 2.0))
                     } else {
-                        if(Random.nextInt(4) == 0) {
+                        if(Random.nextInt(2) == 0) {
                             asteroids.add(Asteroid.createRandomOnBorder(this))
                         }
                     }
