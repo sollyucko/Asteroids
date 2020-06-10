1123a1c89b56987a59ea8a4cf2b11fde75f954da
Solomon Ucko <solly.ucko@gmail.com>
2020-06-10 14:14:36 -0400

    Decrease enemy fire rate as it loses HP

--- src/io/github/sollyucko/ads/a/asteroids/Enemy.kt
+++ src/io/github/sollyucko/ads/a/asteroids/Enemy.kt
@@ -3,11 +3,14 @@ package io.github.sollyucko.ads.a.asteroids
 import io.github.sollyucko.ads.a.asteroids.utils.GameEntity
 import io.github.sollyucko.ads.a.asteroids.utils.geometry.Circle
 import io.github.sollyucko.ads.a.asteroids.utils.geometry.Point
+import kotlin.random.Random
 
 class Enemy : Circle(Point(RADIUS*2, RADIUS*2), RADIUS), GameEntity<Asteroids> {
     override fun tick(game: Asteroids) {
-        val orientation = (game.getPlayerPosition() - anchor).orientation
-        game.addEnemyBullet(Bullet(center, orientation))
+        if(Random.nextInt(START_HP) < hp) {
+            val orientation = (game.getPlayerPosition() - anchor).orientation
+            game.addEnemyBullet(Bullet(center, orientation))
+        }
     }
 
     var hp = START_HP
