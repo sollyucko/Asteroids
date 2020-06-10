b309820a0186c85631dc4e3ca12cbed9caaaa9a7
Solomon Ucko <solly.ucko@gmail.com>
2020-06-10 13:49:14 -0400

    Adjust enemy position

--- src/io/github/sollyucko/ads/a/asteroids/Enemy.kt
+++ src/io/github/sollyucko/ads/a/asteroids/Enemy.kt
@@ -4,7 +4,7 @@ import io.github.sollyucko.ads.a.asteroids.utils.GameEntity
 import io.github.sollyucko.ads.a.asteroids.utils.geometry.Circle
 import io.github.sollyucko.ads.a.asteroids.utils.geometry.Point
 
-class Enemy : Circle(Point(RADIUS, RADIUS), RADIUS), GameEntity<Asteroids> {
+class Enemy : Circle(Point(RADIUS*2, RADIUS*2), RADIUS), GameEntity<Asteroids> {
     override fun tick(game: Asteroids) {
         // TODO: fire bullets at player
     }
